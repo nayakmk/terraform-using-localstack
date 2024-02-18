64f5cc7049c149c4cc1c9a4a59520fc24e98fd73
Malaya Nayak <malaya.nayak@outlook.com>
2024-02-18 23:46:28 +0530

    variables

---
+++ variables/main.tf
@@ -0,0 +1,11 @@
+# Resource Block
+resource "aws_instance" "my-ec2" {
+  ami               = "ami-ff0fea8310f3"
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    "name" : "localstack-ec2"
+    "value" : "my-ec2"
+  }
+}
